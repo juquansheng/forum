4cd5fa4575e1c124469738a1fcf112e8815406a8
juquansheng <juquansheng@hotmail.com>
2021-01-04 10:24:32 +0800

    Mongodb

---
+++ common/mongondb/src/main/java/com/uuuuuuuuuuuuuuu/mongodb/MongodbConfiguration.java
@@ -0,0 +1,9 @@
+package com.uuuuuuuuuuuuuuu.mongodb;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.uuuuuuuuuuuuuuu.mongodb"})
+public class MongodbConfiguration {
+
+
+}
---
+++ common/mongondb/src/test/java/com/uuuuuuuuuuuuuuu/mongodb/MongondbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuuuuuuuuuuuuuu.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongondbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
