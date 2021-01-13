30e4931a2655ccda8fb2de2478390c3bec10e26d
juquansheng <mima33310914>
2021-01-13 22:09:46 +0800

    es

--- business/search/src/main/java/com/uuuuuuuuuuuuuuu/search/controller/ElasticsearchController.java
+++ business/search/src/main/java/com/uuuuuuuuuuuuuuu/search/controller/ElasticsearchController.java
@@ -3,6 +3,10 @@ package com.uuuuuuuuuuuuuuu.search.controller;
 import com.uuuuuuuuuuuuuuu.model.es.entity.BlobESMetaData;
 import com.uuuuuuuuuuuuuuu.model.global.BaseMessageConf;
 import com.uuuuuuuuuuuuuuu.model.vo.Result;
+import com.uuuuuuuuuuuuuuu.search.repository.HighLight;
+import com.uuuuuuuuuuuuuuu.search.repository.PageList;
+import com.uuuuuuuuuuuuuuu.search.repository.PageSortHighLight;
+import com.uuuuuuuuuuuuuuu.search.repository.Sort;
 import com.uuuuuuuuuuuuuuu.search.service.ElasticsearchIndex;
 import com.uuuuuuuuuuuuuuu.search.service.ElasticsearchTemplate;
 import io.swagger.annotations.Api;
@@ -15,6 +19,7 @@ import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,10 +59,30 @@ public class ElasticsearchController {
                              @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer
                                      pageSize) throws Exception {
 
+
+        //分页
+        PageSortHighLight psh = new PageSortHighLight(currentPage,pageSize);
+        //排序字段，注意如果proposal_no是text类型会默认带有keyword性质，需要拼接.keyword
+        String sorter = "proposal_no.keyword";
+        Sort.Order order = new Sort.Order(SortOrder.ASC,sorter);
+        psh.setSort(new Sort(order));
+        //定制高亮，如果定制了高亮，返回结果会自动替换字段值为高亮内容
+        psh.setHighLight(new HighLight().field("keywords"));
+        //可以单独定义高亮的格式
+        //new HighLight().setPreTag("<em>");
+        //new HighLight().setPostTag("</em>");
+        PageList<BlobESMetaData> pageList = new PageList<>();
+        pageList = elasticsearchTemplate.search(new MatchAllQueryBuilder(), psh, BlobESMetaData.class);
+        pageList.getList().forEach(main2 -> System.out.println(main2));
+
         if (StringUtils.isEmpty(keywords)) {
             return Result.failed(BaseMessageConf.KEYWORD_IS_NOT_EMPTY);
         }
 
         return Result.ok(elasticsearchTemplate.search(QueryBuilders.matchQuery("content",keywords),BlobESMetaData.class));
     }
+
+
+
+
 }
