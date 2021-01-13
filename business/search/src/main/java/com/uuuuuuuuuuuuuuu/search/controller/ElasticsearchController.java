94b79f9c5da9c4306deb5ade7c88a676396655fb
juquansheng <mima33310914>
2021-01-13 21:16:30 +0800

    es

--- business/search/src/main/java/com/uuuuuuuuuuuuuuu/search/controller/ElasticsearchController.java
+++ business/search/src/main/java/com/uuuuuuuuuuuuuuu/search/controller/ElasticsearchController.java
@@ -1,12 +1,27 @@
 package com.uuuuuuuuuuuuuuu.search.controller;
 
 import com.uuuuuuuuuuuuuuu.model.es.entity.BlobESMetaData;
+import com.uuuuuuuuuuuuuuu.model.global.BaseMessageConf;
+import com.uuuuuuuuuuuuuuu.model.vo.Result;
 import com.uuuuuuuuuuuuuuu.search.service.ElasticsearchIndex;
 import com.uuuuuuuuuuuuuuu.search.service.ElasticsearchTemplate;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.checkerframework.checker.units.qual.A;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.MatchAllQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * description: ElasticsearchController
@@ -14,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  * author: juquansheng
  * version: 1.0 <br>
  */
+@Api(value = "博客ElasticSearch相关接口", tags = {"博客ElasticSearch相关接口"})
 @RestController
 @RequestMapping(value = "es")
 public class ElasticsearchController {
@@ -29,4 +45,19 @@ public class ElasticsearchController {
         elasticsearchTemplate.save(blobESMetaData);
     }
 
+    @ApiOperation(value = "通过ElasticSearch搜索博客", notes = "通过ElasticSearch搜索博客", response = String.class)
+    @GetMapping("/elasticSearchBlog")
+    public Result searchBlog(HttpServletRequest request,
+                             @RequestParam(required = false) String keywords,
+                             @RequestParam(name = "currentPage", required = false, defaultValue = "1") Integer
+                                     currentPage,
+                             @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer
+                                     pageSize) throws Exception {
+
+        if (StringUtils.isEmpty(keywords)) {
+            return Result.failed(BaseMessageConf.KEYWORD_IS_NOT_EMPTY);
+        }
+
+        return Result.ok(elasticsearchTemplate.search(QueryBuilders.matchQuery("content",keywords),BlobESMetaData.class));
+    }
 }
