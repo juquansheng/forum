198ac5bf15f46d3560bdf998a79678ae2fe86826
juquansheng <mima33310914>
2021-02-24 20:25:48 +0800

    解决微服务client的application问题，是由于application.properties覆盖bootstrap.yml的spring.application.name

--- auth/src/main/java/com/uuuuuuuuuuuuuuu/auth/controller/TestController.java
+++ auth/src/main/java/com/uuuuuuuuuuuuuuu/auth/controller/TestController.java
@@ -33,4 +33,9 @@ public class TestController {
             return "test";
         }
     }
+
+    public static void main(String[] args) {
+        String[] a = new String[1];
+        System.out.println(a[0]);
+    }
 }
--- business/search/src/main/java/com/uuuuuuuuuuuuuuu/search/SearchApplication.java
+++ business/search/src/main/java/com/uuuuuuuuuuuuuuu/search/SearchApplication.java
@@ -2,14 +2,23 @@ package com.uuuuuuuuuuuuuuu.search;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
+import com.uuuuuuuuuuuuuuu.mongodb.MongodbConfiguration;
+import com.uuuuuuuuuuuuuuu.mongodb.config.MongoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude= {DruidDataSourceAutoConfigure.class, DynamicDataSourceAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude= {DruidDataSourceAutoConfigure.class,
+        DynamicDataSourceAutoConfiguration.class,
+        MongoDataAutoConfiguration.class,
+        MongoAutoConfiguration.class,
+        MongodbConfiguration.class,
+        DataSourceAutoConfiguration.class})
 @EnableScheduling
 @EnableEurekaClient
 @EnableFeignClients(basePackages={"com.uuuuuuuuuuuuuuu.feign.feign"})
